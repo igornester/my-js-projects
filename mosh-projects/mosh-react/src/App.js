6708df120255afa95932a507a06b48517d58a13a
Alex <Jurchenko.a@gmail.com>
2020-06-29 22:10:30 +0300

    rename folders ith Mosh projects

---
+++ mosh-projects/mosh-react/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Counters from "./components/counters";
+
+function App() {
+  return (
+    <div className="App">
+      <Counters />
+    </div>
+  );
+}
+
+export default App;
