082a8d69699e51709332acc2daf209fdf36db3d1
igornester <48754509+igornester@users.noreply.github.com>
2020-07-16 19:22:17 +0300

    Update index.js

--- mosh-projects/mongo-demo/index.js
+++ mosh-projects/mongo-demo/index.js
@@ -1,23 +1,18 @@
 const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost/alexdb")
-  .then(() => {
-    console.log("Mongo DB connected..");
-  })
-  .catch((e) => {
-    console.error(`Connection error ${e}`);
-  });
-
-const courseSchema = new mongoose.Schema({
-  title: String,
-  tags: [String],
-  date: { type: Date, default: Date.now },
-  isPublished: Boolean,
-});
-
-const Course = mongoose.model("Course", courseSchema);
 
 async function createCourse() {
+
+  await mongoose.connect("mongodb://localhost/alexdb")
+
+  const courseSchema = new mongoose.Schema({
+    title: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean,
+  });
+
+  const Course = mongoose.model("Course", courseSchema);
+
   const course = new Course({
     title: "New course",
     tags: ["test", "new", "course"],
